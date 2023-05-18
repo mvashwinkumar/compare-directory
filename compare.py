81fb676f77c08cdba29643239704e5b11f5cb303
Ashwin Kumar M V <mvashwinkumar@gmail.com>
2023-05-18 15:34:28 +0800

    Update compare.py
    - add support for tags
    - show filter with total and visible count when applicable

--- compare.py
+++ compare.py
@@ -129,7 +129,55 @@ def merge_str_diff(str, cdiff):
             result += html.escape(str[i])
     return result
 
-def compare_dirs(dir1, dir2, output_file, ignore_file_extensions=[], nlines=3):
+def parse_tag_file_list(tag_file_list):
+    tag_dict = {}
+    for tag_file in tag_file_list:
+        tag, file_list = tag_file.split(':')
+        tag_dict[tag] = file_list.split(',')
+    return tag_dict
+
+def transform_file_tags_dict(tag_files_dict):
+    file_tags_dict = {}
+    for tag, files in tag_files_dict.items():
+        for file in files:
+            if file in file_tags_dict:
+                file_tags_dict[file].append(tag)
+            else:
+                file_tags_dict[file] = [tag]
+    return file_tags_dict
+
+def generate_file_path_td(file_path, file_tags_dict):
+    file_path_td = f"<td class='ten'><span class='content'>{file_path}</span></td>"
+    tags = []
+    for file_path_key, tag_list in file_tags_dict.items():
+        if file_path_key in file_path:
+            tags.extend(tag_list)
+    file_tags = set()
+    for tag in tags:
+        file_tags.add(tag)
+
+    if len(file_tags) > 0:
+        file_tags_span_list = [f"<span class='tags'>{tag}</span>" for tag in file_tags]
+        file_path_td = f"<td class='ten'><span class='content'>{file_path}</span>{''.join(file_tags_span_list)}</td>"
+    
+    return file_path_td
+
+def compare_dirs(dir1, dir2, output_file, ignore_file_extensions=[], nlines=3, tags=[]):
+    tag_files_dict = parse_tag_file_list(tags)
+    all_tags = set()
+    for tag, file_list in tag_files_dict.items():
+        all_tags.add(tag)
+    
+    file_tags_dict = transform_file_tags_dict(tag_files_dict)
+
+    tag_buttons_html_list = [f"<button onclick='onfilterByTag(this, \"{tag}\")' class='stats-button tag-button'>{tag}</button>" for tag in all_tags]
+    tags_filter_div = f"""
+        <div id="tags-filter-div" style="display: flex; justify-content: space-around; align-items: center; margin: 10px 0px; padding: 0.75rem; border: solid 2px black;">
+            <div style='padding: 10px; font-weight: bold; text-align: center;'>Tags Available</div>
+            {''.join(tag_buttons_html_list)}
+        </div>
+    """ if len(all_tags) > 0 else ""
+
     stats = {
         'total': 0,
         'ignored': 0,
@@ -173,6 +221,14 @@ def compare_dirs(dir1, dir2, output_file, ignore_file_extensions=[], nlines=3):
         }
         .ten {
             vertical-align: top;
+        }
+        .ten .tags {
+            display: inline-block;
+            margin-left: 10px;
+            padding: 5px;
+            border-radius: 5px;
+            background: #4f0054e0;
+            color: white;
         }
         .twenty {
             width: 37.5%;
@@ -221,63 +277,6 @@ def compare_dirs(dir1, dir2, output_file, ignore_file_extensions=[], nlines=3):
     <div style="display: inline-block;">Search by file path / extn:</div>
     <input type="text" id="searchInput" list="filePaths" onkeyup="filterRows()" placeholder="Enter your text here...">
     <datalist id="filePaths"></datalist>
-    <script>
-        function getActiveStats() {
-            let statsDiv = document.getElementById('stats-div');
-            let buttons = statsDiv.getElementsByTagName('button');
-            let activeClasses = [];
-            for (let i = 0; i < buttons.length; i++) {
-                if (buttons[i].classList.contains('active')) {
-                    activeClasses.push(buttons[i].classList[1]);
-                }
-            }
-            return activeClasses;
-        }
-
-        function updateSuggestions(value, activeStats) {
-            let dataList = document.getElementById('filePaths');
-            dataList.innerHTML = '';
-            let table = document.getElementById('comparisonTable');
-            let tr = document.querySelectorAll('#comparisonTable > tbody > tr');
-            
-            for (let i = 0; i < tr.length; i++) {
-                let td = tr[i].getElementsByTagName('td')[1];
-                let showByStatsFilter = activeStats.length > 0 ? activeStats.includes(tr[i].classList[0]): true;
-                if (td) {
-                    let txtValue = td.textContent || td.innerText;
-                    if (showByStatsFilter && txtValue.toUpperCase().includes(value.toUpperCase())) {
-                        let option = document.createElement('option');
-                        option.value = txtValue.trim();
-                        dataList.appendChild(option);
-                    }
-                }
-            }
-        }
-        function filterRows() {
-            let input = document.getElementById('searchInput');
-            let filterText = input.value.toUpperCase();
-            let activeStats = getActiveStats();
-
-            let tr = document.querySelectorAll('#comparisonTable > tbody > tr');
-
-            for (let i = 0; i < tr.length; i++) {
-                let td = tr[i].getElementsByTagName('td')[1];
-                let showByStatsFilter = activeStats.length > 0 ? activeStats.includes(tr[i].classList[0]): true;
-                if (td) {
-                    let txtValue = td.textContent || td.innerText;
-                    if (showByStatsFilter && txtValue.toUpperCase().indexOf(filterText) > -1) {
-                        tr[i].style.display = '';
-                    } else {
-                        tr[i].style.display = 'none';
-                    }
-                }
-            }
-
-            if (input.value.length >= 3) {
-                updateSuggestions(input.value, activeStats);
-            }
-        }
-    </script>
     """
 
     table_header = f"""
@@ -304,17 +303,19 @@ def compare_dirs(dir1, dir2, output_file, ignore_file_extensions=[], nlines=3):
             file_path1 = os.path.join(root1, file1)
             file_path2 = os.path.join(root2, file2)
 
+            file_path_td = generate_file_path_td(file_path1, file_tags_dict)
+
             if not os.path.exists(file_path2):
                 stats['removed'] += 1
-                table_rows.append(f"<tr class='file-removed'><td class='small'></td><td class='ten'>{file_path1}</td><td class='twenty' colspan='2' style='text-align: center;'><span>Removed from '{dir2}'</span></td></tr>")
+                table_rows.append(f"<tr class='file-removed'><td class='small'></td>{file_path_td}<td class='twenty' colspan='2' style='text-align: center;'><span>Removed from '{dir2}'</span></td></tr>")
                 continue
             
             if filecmp.cmp(file_path1, file_path2, shallow=False):
                 stats['identical'] += 1
-                table_rows.append(f"<tr class='file-no-change'><td class='small'></td><td class='ten'>{file_path1}</td><td class='twenty' colspan='2' style='text-align: center;'><span>No change</span></td></tr>")
+                table_rows.append(f"<tr class='file-no-change'><td class='small'></td>{file_path_td}<td class='twenty' colspan='2' style='text-align: center;'><span>No change</span></td></tr>")
             elif os.path.splitext(file_path1)[1][1:] in ignore_file_extensions:
                 stats['ignored'] += 1
-                table_rows.append(f"<tr class='file-ignored'><td class='small'></td><td class='ten'>{file_path1}</td><td class='twenty'><span>{get_file_properties_table(file_path1, show_md5_hash=True)}</span></td><td class='twenty'><span>{get_file_properties_table(file_path2, show_md5_hash=True)}</span></td></tr>")
+                table_rows.append(f"<tr class='file-ignored'><td class='small'></td>{file_path_td}<td class='twenty'><span>{get_file_properties_table(file_path1, show_md5_hash=True)}</span></td><td class='twenty'><span>{get_file_properties_table(file_path2, show_md5_hash=True)}</span></td></tr>")
             else:
                 stats['changed'] += 1
                 with open(file_path1, encoding='utf8') as f1, open(file_path2, encoding='utf8') as f2:
@@ -351,7 +352,7 @@ def compare_dirs(dir1, dir2, output_file, ignore_file_extensions=[], nlines=3):
                     table_rows.append(f"""
                     <tr class='file-changed'>
                         <td class='small'><span class="collapse-icon" onclick="toggleRow(this.parentElement.parentElement, this)" style="cursor:pointer;">[-]</span></td>
-                        <td class='ten'>{file_path1}</td>
+                        {file_path_td}
                         <td class='twenty'>
                             {get_file_properties_table(file_path1)}
                             {'<br>'.join(diff1)}
@@ -370,16 +371,18 @@ def compare_dirs(dir1, dir2, output_file, ignore_file_extensions=[], nlines=3):
             file_path1 = os.path.join(root1, file1)
             file_path2 = os.path.join(root2, file2)
 
+            file_path_td = generate_file_path_td(file_path2, file_tags_dict)
+
             if not os.path.exists(file_path1):
                 stats['added'] += 1
-                table_rows.append(f"<tr class='file-added'><td class='small'></td><td class='ten'>{file_path2}</td><td class='twenty' colspan='2' style='text-align: center;'><span>Added in '{dir2}'</span></td></tr>")
+                table_rows.append(f"<tr class='file-added'><td class='small'></td>{file_path_td}<td class='twenty' colspan='2' style='text-align: center;'><span>Added in '{dir2}'</span></td></tr>")
 
     table_footer = "</tbody></table>"
 
     stats['total'] = stats['identical'] + stats['changed'] + stats['added'] + stats['removed'] + stats['ignored']
     stats_div = f"""
         <div id="stats-div" style="display: flex; justify-content: space-around; align-items: center; margin: 10px 0px; padding: 0.75rem; border: solid 2px black;">
-            <div style='padding: 10px; font-weight: bold; text-align: center;'>Total: {stats['total']}</div>
+            <div style='padding: 10px; font-weight: bold; text-align: center;' id="visible-rows-stat" data-total="{stats['total']}">Total: {stats['total']}</div>
             <button onclick="onfilterByStats(this, 'file-changed')" class='stats-button file-changed'>Text changed: {stats['changed']}</button>
             <button onclick="onfilterByStats(this, 'file-ignored')" class='stats-button file-ignored'>Hash changed: {stats['ignored']}</button>
             <button onclick="onfilterByStats(this, 'file-added')" class='stats-button file-added'>Added: {stats['added']}</button>
@@ -390,7 +393,107 @@ def compare_dirs(dir1, dir2, output_file, ignore_file_extensions=[], nlines=3):
 
     script_tag = """
         <script>
+            function getActiveStats() {
+                let statsDiv = document.getElementById('stats-div');
+                let buttons = statsDiv.getElementsByTagName('button');
+                let activeClasses = [];
+                for (let i = 0; i < buttons.length; i++) {
+                    if (buttons[i].classList.contains('active')) {
+                        activeClasses.push(buttons[i].classList[1]);
+                    }
+                }
+                return activeClasses;
+            }
+
+            function getActiveTags() {
+                let tagsFilterDiv = document.getElementById('tags-filter-div');
+                if (!tagsFilterDiv) {
+                    return [];
+                }
+
+                let buttons = tagsFilterDiv.getElementsByTagName('button');
+                let activeClasses = [];
+                for (let i = 0; i < buttons.length; i++) {
+                    if (buttons[i].classList.contains('active')) {
+                        activeClasses.push(buttons[i].textContent);
+                    }
+                }
+                return activeClasses;
+            }
+
+            function getFilePathFromTableRow(tr) {
+                const [expCollapseTd, filePathTd] = tr.getElementsByTagName('td');
+                if (filePathTd) {
+                    const [filePathSpan, tagsSpan] = filePathTd.getElementsByTagName('span');
+                    return filePathSpan.textContent || filePathSpan.innerText;
+                }
+                return '';
+            }
+
+            function shouldShowRow(tr, activeStats, activeTags) {
+                const showByStatsFilter = activeStats.length > 0 ? activeStats.includes(tr.classList[0]): true;
+
+                const [expCollapseTd, filePathTd] = tr.getElementsByTagName('td');
+                const [, ...tagsSpanList] = filePathTd.getElementsByTagName('span');
+                const rowTags = tagsSpanList.map(span => span.textContent);
+                const showByTagsFilter = activeTags.length > 0 ? activeTags.every(tag => rowTags.includes(tag)): true;
+
+                return showByStatsFilter && showByTagsFilter;
+            }
+
+            function updateSuggestions(value, activeStats, activeTags) {
+                let dataList = document.getElementById('filePaths');
+                dataList.innerHTML = '';
+                let table = document.getElementById('comparisonTable');
+                let tr = document.querySelectorAll('#comparisonTable > tbody > tr');
+                
+                for (let i = 0; i < tr.length; i++) {
+                    const showRowByFilter = shouldShowRow(tr[i], activeStats, activeTags)
+
+                    let txtValue = getFilePathFromTableRow(tr[i]);
+                    if (showRowByFilter && txtValue.toUpperCase().includes(value.toUpperCase())) {
+                        let option = document.createElement('option');
+                        option.value = txtValue.trim();
+                        dataList.appendChild(option);
+                    }
+                }
+            }
+            function filterRows() {
+                let input = document.getElementById('searchInput');
+                let filterText = input.value.toUpperCase();
+                let activeStats = getActiveStats();
+                let activeTags = getActiveTags();
+
+                let tr = document.querySelectorAll('#comparisonTable > tbody > tr');
+                let visibleRowCount = 0;
+                for (let i = 0; i < tr.length; i++) {
+                
+                    const showRowByFilter = shouldShowRow(tr[i], activeStats, activeTags)
+
+                    let txtValue = getFilePathFromTableRow(tr[i]);
+                    if (showRowByFilter && txtValue.toUpperCase().indexOf(filterText) > -1) {
+                        tr[i].style.display = '';
+                        visibleRowCount++;
+                    } else {
+                        tr[i].style.display = 'none';
+                    }
+                }
+
+                if (input.value.length >= 3) {
+                    updateSuggestions(input.value, activeStats, activeTags);
+                }
+
+                let visibleRowsStat = document.getElementById('visible-rows-stat');
+                const totalRows = visibleRowsStat.getAttribute('data-total');
+                visibleRowsStat.innerHTML = totalRows === visibleRowCount.toString() ? `Total: ${totalRows}` : `Total: ${totalRows} | Visible: ${visibleRowCount}`;
+            }
             function onfilterByStats($this, className) {
+                $this.classList.toggle('active')
+                filterRows()
+            }
+
+            function onfilterByTag($this, tagName) {
+                console.log(tagName)
                 $this.classList.toggle('active')
                 filterRows()
             }
@@ -451,6 +554,7 @@ def compare_dirs(dir1, dir2, output_file, ignore_file_extensions=[], nlines=3):
             <body>
                 <h1 style='text-align: center; width: 100%;'>Directory Comparison Report</h1>
                 {stats_div}
+                {tags_filter_div}
                 {search_bar}
                 <hr>
                 {table_header}
@@ -505,13 +609,13 @@ def process_csv(csv_file, ignore_file_extensions=[], nlines=3, index='difference
         csv_reader = csv.reader(csvfile)
         next(csv_reader)  # Skip the first row (labels)
         for row in csv_reader:
-            if len(row) != 3:
-                raise ValueError('Invalid CSV format. Each row should have exactly 3 values: dir1, dir2, output.')
-            dir1, dir2, output = row
+            if len(row) != 4:
+                raise ValueError('Invalid CSV format. Each row should have exactly 4 values: dir1, dir2, output, tags.')
+            dir1, dir2, output, tags = row
             print(f'Comparing {dir1} and {dir2} and generating {output}')
             # get the start time
             st = time.time()
-            compare_dirs(dir1, dir2, output, ignore_file_extensions, nlines)
+            compare_dirs(dir1, dir2, output, ignore_file_extensions, nlines, tags=tags.split(';'))
             # get the end time
             et = time.time()
             # get the execution time
@@ -519,15 +623,19 @@ def process_csv(csv_file, ignore_file_extensions=[], nlines=3, index='difference
             print(f'Execution time for {output}:', elapsed_time, 'seconds')
             html_files.append((dir1, dir2, output))
     create_index_html(html_files, index)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Compare two directories and generate an HTML report. Provide either dir1 and dir2 or a CSV file with multiple sets of arguments (dir1, dir2, output).',
-        usage='''python compare_directories.py path/to/first/directory path/to/second/directory --hash war jar -o my_differences.html
-              OR
-              python compare_directories.py --csv path/to/csv_file.csv --hash war jar -n 3 --index differences_index.html
-              Example CSV Format:
-              dir1,dir2,output
-              path/to/first/directory,path/to/second/directory,my_differences.html'''
+        usage='''
+    ------------------------------------
+    python compare_directories.py path/to/first/directory path/to/second/directory --hash war jar --tags tag1:/sub-path/to/filename1,/sub-path/to/filename2 tag2:/sub-path/to/filename2,/sub-path/to/filename3 -o my_differences.html
+        (or)
+    python compare_directories.py --csv path/to/csv_file.csv --hash war jar -n 3 --index differences_index.html
+    ------------------------------------
+    CSV Format:
+    dir1,dir2,output,tags
+    path/to/first/directory,path/to/second/directory,my_differences.html,tag1:/sub-path/to/filename1,/sub-path/to/filename2;tag2:/sub-path/to/filename2,/sub-path/to/filename3'''
     )
     parser.add_argument('--csv', help='Path to the CSV file containing multiple sets of arguments.')
     parser.add_argument('--index', default='differences_index.html', help='Path to the output file (default: differences_index.html).')
@@ -535,6 +643,7 @@ if __name__ == "__main__":
     parser.add_argument('dir2', nargs='?', help='Path to the second directory.')
     parser.add_argument('-o', '--output', default='differences.html', help='Path to the output file (default: differences.html).')
     parser.add_argument('--hash', nargs='+', default=['war', 'jar', 'jks'], help='List of file extensions to do MD5 Hash Compare (default: war jar jks).')
+    parser.add_argument('--tags', nargs='+', default=[], help='List of tags to associate with matching file paths in format: tag1:/sub-path/to/filename1,/sub-path/to/filename2 (default: []).')
     parser.add_argument('-n', '--nlines', type=int, default=3, help='Number of unchanged lines to show above and below diff (default: 3).')
 
     args = parser.parse_args()
@@ -547,7 +656,7 @@ if __name__ == "__main__":
     else:
         if not args.dir1 or not args.dir2:
             parser.error("Following arguments are required: dir1, dir2")
-        compare_dirs(args.dir1, args.dir2, args.output, ignore_file_extensions=args.hash, nlines=args.nlines)
+        compare_dirs(args.dir1, args.dir2, args.output, ignore_file_extensions=args.hash, nlines=args.nlines, tags=args.tags)
     # get the end time
     tet = time.time()
     # get the execution time
